4b5d414f8156e2de78f86c9006fd743346094ec6
trippymajo <elyadesu@yandex.ru>
2025-11-21 14:21:09 +0200

    WIP.Task #4. C++ ping example init state.

---
+++ c-abi-libp2p/examples/cpp/ping.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <cerrno>
+
+// Crossplatform
+#ifdef WIN32
+#include <windows.h>
+using LibHandle = HMODULE;
+#define LOAD_LIB(path) LoadLibraryA(path)
+#define GET_PROC(lib, name) GetProcAddress(lib, name)
+#define CLOSE_LIB(lib) FreeLibrary(lib)
+constexpr auto LIB_NAME = "cabi_rust_libp2p.dll";
+#else
+#include <dlfcn.h>
+using LibHandle = void*;
+#define LOAD_LIB(path) dlopen(path, RTLD_LAZY)
+#define GET_PROC(lib, name) dlsym(lib, name)
+#define CLOSE_LIB(lib) dlclose(lib)
+constexpr auto LIB_NAME = "cabi_rust_libp2p.so"
+#endif
+
+using std::cout;
+using std::cerr;
+
+// Operation completed successfully.
+constexpr int CABI_STATUS_SUCCESS = 0;
+// One of the provided pointers was null.
+constexpr int CABI_STATUS_NULL_POINTER = 1;
+// Invalid argument supplied (e.g. malformed multiaddr).
+constexpr int CABI_STATUS_INVALID_ARGUMENT = 2;
+// Internal runtime error – check logs for details.
+constexpr int CABI_STATUS_INTERNAL_ERROR = 3;
+
+using InitTracingFunc = int (*)();
+using NewNodeFunc = void* (*)(bool useQuic);
+using ListenNodeFunc = int (*)(void* handle, const char* multiaddr);
+using DialNodeFunc = int (*)(void* handle, const char* multiaddr);
+using FreeNodeFunc = void (*)(void* handle);
+
+struct CabiRustLibp2p
+{
+  InitTracingFunc InitTracing{};
+  NewNodeFunc     NewNode{};
+  ListenNodeFunc  ListenNode{};
+  DialNodeFunc    DialNode{};
+  FreeNodeFunc    FreeNode{};
+};
+
+bool loadAbi(LibHandle lib, CabiRustLibp2p& abi)
+{
+  abi.InitTracing = reinterpret_cast<InitTracingFunc>(GET_PROC(lib, "cabi_init_tracing"));
+  abi.NewNode = reinterpret_cast<NewNodeFunc>(GET_PROC(lib, "cabi_node_new"));
+  abi.ListenNode = reinterpret_cast<ListenNodeFunc>(GET_PROC(lib, "cabi_node_listen"));
+  abi.DialNode = reinterpret_cast<DialNodeFunc>(GET_PROC(lib, "cabi_node_dial"));
+  abi.FreeNode = reinterpret_cast<FreeNodeFunc>(GET_PROC(lib, "cabi_node_free"));
+
+  return  abi.InitTracing && abi.NewNode &&
+          abi.ListenNode && abi.DialNode && abi.FreeNode;
+}
+
+int main()
+{
+  LibHandle lib = LOAD_LIB(LIB_NAME);
+  if (!lib)
+  {
+    cerr << "Error on loading Lib:" << LIB_NAME << "\n";
+    return 1;
+  }
+
+  // Try get functions from library
+  CabiRustLibp2p abi;
+  if (loadAbi(lib, abi))
+  {
+    cerr << "Missing required functions in library \n";
+  }
+
+  // Try init cabi's tracing
+
+
+
+  CLOSE_LIB(lib);
+  return 0;
+}
